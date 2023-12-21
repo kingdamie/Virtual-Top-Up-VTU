739328ad85dfbf1608fb6da437a3bbc7e9a8ee66
kingdamie <damyridwan@gmail.com>
2023-12-21 17:33:56 +0100

    updated

--- buy-data.php
+++ buy-data.php
@@ -16,8 +16,9 @@
 	<link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="css/session.css">
     <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/buydata.css">
 
-	<title>Dashboard</title>
+	<title>Buy Data</title>
 </head>
 <body>
 	<!-- sidebar  -->
@@ -35,14 +36,53 @@
 			<div class="head-title">
 				<div class="left">
 					<h1>Buy Data</h1>
-					<ul class="breadcrumb">
-						<li>
-							<a href="#">Electricity Bill</a>
-						</li>
-						<li><i class='bx bx-chevron-right' ></i></li>
-					</ul>
 				</div>
 			</div>
+            <form action="">
+                <div class="box-info">
+                        <div class="network">
+                            <h4>Phone Number:</h4>
+                            <div class="box">
+                                <span>+234</span>
+                                <input type="number">
+                            </div>
+                        </div>
+                </div>
+
+                <div class="box-info">
+                        <div class="network nt">
+                            <h4>Network:</h4>
+                            <div class="box" style="cursor: not-allowed;">
+                                <span>MTN</span>
+                            </div>
+                        </div>
+                </div>
+
+                <div class="box-info">
+                        <div class="network nt">
+                            <h4>Data Bundle:</h4>
+                                <select class="box" id="customSelect" name="customSelect">
+                                    <option value="option1">mtn₦100 = 100MB</option>
+                                    <option value="option2">₦200 = 200MB</option>
+                                    <option value="option3">₦350 = 1GB</option>
+                                    <option value="option4">₦300 = 750MB</option>
+                                    <option value="option5">₦600 = 2GB</option>
+                                    <option value="option6">₦1000 = 3GB</option>
+                            </select>
+                        </div>
+                </div>
+
+                <div class="box-info">
+                        <div class="network nt">
+                            <h4>Amount:</h4>
+                            <div class="box" style="cursor: not-allowed;">
+                                <span>2,000.00</span>
+                            </div>
+                        </div>
+                </div>
+
+                <center><input type="submit" class="primary-btn" value="Buy"></center>
+            </form>
 		</main>
 		<!-- MAIN -->
 	</section>
